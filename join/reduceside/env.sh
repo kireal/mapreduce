50fac9ca865f511b70aa2ff44512d037e60886c3
Kirill Chufarov <kirillchufarov@gmail.com>
2013-08-31 22:55:28 +0400

    env merge

--- join/reduceside/env.sh
+++ join/reduceside/env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-export LeftDSDelim=";" #delimeter in left data set
-export RightDSDelim=";" #delimeter in left data set
-export DSColCounts="3, 2" #column count in each dataset
-export SelectIDX="0,1,2;0,1" #-1 means select all column from dataset
-export JoinKeyIDX="1; 1" #it means join by first column by default
-export JoinType="Full"
+export LeftDSDelim="," #delimeter in left data set
+export RightDSDelim="," #delimeter in left data set
+export DSColCounts="3, 4" #column count in each dataset
+export SelectIDX="0,1,2;0,1,2,3" #-1 means select all column from dataset
+export JoinKeyIDX="0;0" #it means join by first column by default
+export JoinType="Inner"
